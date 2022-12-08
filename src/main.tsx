4508c228c293ebc648a996340d0af31146e01202
Luís Gomes <luis252001@hotmail.com>
2022-12-08 18:15:33 -0300

    chore: remove unused css file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AppContextProvider } from './contexts/AppContext'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <AppContextProvider>
